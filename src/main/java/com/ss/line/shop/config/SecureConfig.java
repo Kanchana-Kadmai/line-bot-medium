f8db389056ad18955accd150cf8f83216d8886da
May Kanchana <kanchana.khadmai@gmail.com>
2020-02-01 00:28:38 +0700

    no message

--- src/main/java/com/ss/line/shop/config/SecureConfig.java
+++ src/main/java/com/ss/line/shop/config/SecureConfig.java
@@ -1,28 +1,28 @@
-package com.ss.line.shop.config;
+// package com.ss.line.shop.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.HttpMethod;
+// import org.springframework.security.config.annotation.web.builders.WebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
-@EnableAutoConfiguration
-public class SecureConfig extends WebSecurityConfigurerAdapter {
+// @Configuration
+// @EnableWebSecurity
+// @EnableAutoConfiguration
+// public class SecureConfig extends WebSecurityConfigurerAdapter {
 
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-		.antMatchers(HttpMethod.OPTIONS)
-		.antMatchers("/resources/**")
-		.antMatchers("/webjars/**")
-		.antMatchers("/swagger**")
-		.antMatchers("/swagger-resources/**")
-		.antMatchers("/v2/api-docs")
-		.antMatchers("/localize/**")
-		.antMatchers("/localize/**")
-		.antMatchers("/callback/**")
-		.antMatchers("/line/**");
-	}
-}
+// 	public void configure(WebSecurity web) throws Exception {
+// 		web.ignoring()
+// 		.antMatchers(HttpMethod.OPTIONS)
+// 		.antMatchers("/resources/**")
+// 		.antMatchers("/webjars/**")
+// 		.antMatchers("/swagger**")
+// 		.antMatchers("/swagger-resources/**")
+// 		.antMatchers("/v2/api-docs")
+// 		.antMatchers("/localize/**")
+// 		.antMatchers("/localize/**")
+// 		.antMatchers("/callback/**")
+// 		.antMatchers("/line/**");
+// 	}
+// }
--- src/main/java/com/ss/line/shop/core/utils/UserUtils.java
+++ src/main/java/com/ss/line/shop/core/utils/UserUtils.java
@@ -1,22 +1,22 @@
-package com.ss.line.shop.core.utils;
+// package com.ss.line.shop.core.utils;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class UserUtils {
+// public class UserUtils {
 	
-	public static String getUsername() {
-		return SecurityContextHolder.getContext().getAuthentication().getName();
-	}
+// 	public static String getUsername() {
+// 		return SecurityContextHolder.getContext().getAuthentication().getName();
+// 	}
 	
-	public static String getCompanyCode() {
-		return "000";
-	}
+// 	public static String getCompanyCode() {
+// 		return "000";
+// 	}
 	
-	public static String hashPassword(String passWord) throws Exception {
-		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String hashPassword = passwordEncoder.encode(passWord);
-		return hashPassword;
-	}
-}
+// 	public static String hashPassword(String passWord) throws Exception {
+// 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+// 		String hashPassword = passwordEncoder.encode(passWord);
+// 		return hashPassword;
+// 	}
+// }
